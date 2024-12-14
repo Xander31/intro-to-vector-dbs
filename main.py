3df1a74674c8c24c75317bedcbadcea5b0220e56
Xander <alvaro13espinoza@gmail.com>
2024-12-14 17:20:57 -0500

    RAG with LCEL finished

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
 from openai import embeddings
+from langchain_core.runnables import RunnablePassthrough
 
 load_dotenv()
+
+def format_docs (docs) -> str:
+    return "\n\n".join(doc.page_content for doc in docs)
 
 if __name__ == "__main__":
     print("Retrieving...")
@@ -38,3 +42,28 @@ if __name__ == "__main__":
 
     result_retrieval = retrieval_chain.invoke(input={"input": query})
     print(result_retrieval["answer"])
+
+    #2nd implementation - LCEL (Langchain Expression Language)
+
+    template = """Use the following pieces of context to answer the question at the end.
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    Use three sentences maximum and keep the answer as conscise as possible.
+    Always say "thanks for asking" at the end of the answer.
+    
+    {context}
+    
+    Question: {question}
+    
+    Helpful Answer:
+    """
+
+    custom_rag_prompt = PromptTemplate.from_template(template)
+
+    rag_chain = (
+        {"context": vectorstore.as_retriever() | format_docs, "question": RunnablePassthrough()}
+        | custom_rag_prompt
+        | llm
+    )
+
+    res = rag_chain.invoke(input=query)
+    print(res.content)
